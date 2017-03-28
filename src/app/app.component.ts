3c796f24a24c2c7dc208753fd1e7d1f7360707b9
Hazrat Ali <iloveyii@yahoo.com>
2017-03-28 23:34:40 +0200

    added other fields to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>{{name}}</h1>
+  <p><strong>Email:</strong> {{email}}</p>
+  <p><strong>Address:</strong> {{address.street}} {{address.city}}, {{address.state}}</p>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'John Doe';
+  email = 'johnDoe@email.com';
+  address = {
+    street: '12 Main st',
+    city: 'Bromma',
+    state: 'ST'
+  }
+}
