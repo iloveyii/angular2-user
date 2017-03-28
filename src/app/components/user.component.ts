7bb6e80cc18992f2589b266a38b1c37827fcc72a
Hazrat Ali <iloveyii@yahoo.com>
2017-03-29 00:42:43 +0200

    Added addHobby

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -15,7 +15,26 @@ import { Component } from '@angular/core';
                 {{hobby}}
                 </li>
             </ul>
+            <form (submit)="addHobby(hobby.value)">
+                <label>Add Hobby:</label>
+                <input type="text" #hobby /><br />
+            </form>
         </div>
+
+        <hr />
+        <form>
+            <label>Name:</label>
+            <input type="text" name="name" [(ngModel)]="name" /><br />
+
+            <label>Email:</label>
+            <input type="text" name="email" [(ngModel)]="email" /><br />
+
+            <label>City:  </label>
+            <input type="text" name="address.city" [(ngModel)]="address.city" /><br />
+
+            <label>State: </label>
+            <input type="text" name="address.state" [(ngModel)]="address.state" /><br />
+        </form>
   `
 })
 
@@ -44,6 +63,12 @@ export class UserComponent  {
     toggleHobbies() {
         this.showHobbies = ! this.showHobbies;
         this.buttonShowHobbyText = this.showHobbies ? 'Hide Hobbies' : 'Show Hobbies';
+    }
+
+    addHobby(hobby) {
+        if(hobby.length > 0) {
+            this.hobbies.push(hobby);
+        }
     }
 }
 
