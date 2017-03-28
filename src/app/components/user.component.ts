a247f82d02d799c2b45e992c2cfe9bf74f59a3f2
Hazrat Ali <iloveyii@yahoo.com>
2017-03-29 00:11:13 +0200

    added interface to user.component

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -3,22 +3,52 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'user',
     template: `
-      <h1>{{name}}</h1>
-      <p><strong>Email:</strong> {{email}}</p>
-      <p><strong>Address:</strong> {{address.street}} {{address.city}}, {{address.state}}</p>
+        <h1>{{name}}</h1>
+        <p><strong>Email:</strong> {{email}}</p>
+        <p><strong>Address:</strong> {{address.street}} {{address.city}}, {{address.state}}</p>
+
+        <button (click)="toggleHobbies()">{{buttonShowHobbyText}}</button>
+        <div *ngIf="showHobbies">
+            <h3>Hobbies</h3>
+            <ul>
+                <li *ngFor="let hobby of hobbies">
+                {{hobby}}
+                </li>
+            </ul>
+        </div>
   `
 })
 
 export class UserComponent  {
-    name = 'John Doe';
-    email = 'johnDoe@email.com';
-    address = {
-        street: '12 Main st',
-        city: 'Bromma',
-        state: 'ST'
-    }
+    name: string;
+    email: string;
+    address: address;
+    hobbies: string[];
+    showHobbies: boolean;
+    buttonShowHobbyText: string;
 
     constructor() {
         this.name = 'Alex Kan';
+        this.email = 'johnDoe@email.com';
+        this.address = {
+            street: '12 Main st',
+            city: 'Bromma',
+            state: 'ST'
+        };
+        this.hobbies = ['Music', 'Movies', 'Sports'];
+        this.showHobbies = false;
+        this.buttonShowHobbyText = 'Show Hobbies';
+
     }
+
+    toggleHobbies() {
+        this.showHobbies = ! this.showHobbies;
+        this.buttonShowHobbyText = this.showHobbies ? 'Hide Hobbies' : 'Show Hobbies';
+    }
+}
+
+interface address {
+    street: string;
+    city: string;
+    state: string;
 }
